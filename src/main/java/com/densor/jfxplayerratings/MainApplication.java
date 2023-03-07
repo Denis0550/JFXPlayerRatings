10fe7f168d49a30b8bd0effebcd610fdf7a75d01
Denis <denis.soroka5@pm.me>
2023-03-07 23:39:26 +0200

    Successfull writing to db, base for project

--- src/main/java/com/densor/jfxplayerratings/MainApplication.java
+++ src/main/java/com/densor/jfxplayerratings/MainApplication.java
@@ -11,17 +11,26 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        HibernateUtil.getSessionFactory();
-
+        var session = HibernateUtil.getSessionFactory();
 
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("main-view.fxml"));
+
+        var mainController = new MainController(session);
+        fxmlLoader.setController(mainController);
+
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
 
-        HibernateUtil.shutdown();
 
+
+    }
+
+    @Override
+    public void stop() throws Exception {
+        HibernateUtil.shutdown();
+        super.stop();
     }
 
     public static void main(String[] args) {
--- src/main/java/com/densor/jfxplayerratings/MainController.java
+++ src/main/java/com/densor/jfxplayerratings/MainController.java
@@ -1,21 +1,49 @@
 package com.densor.jfxplayerratings;
 
+import com.densor.jfxplayerratings.entity.Ratings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import org.hibernate.SessionFactory;
 
 public class MainController {
+
+    private SessionFactory session;
 
     @FXML
     private Button btn1;
 
     @FXML
-    private Label lbl1;
+    private Button btn2;
 
     @FXML
-    void Clicks(ActionEvent event) {
-        lbl1.setText("testing connection");
+    private TextField textInput;
+
+    @FXML
+    private Label lbl1;
+
+    public MainController(SessionFactory session) {
+        this.session = session;
 
     }
+
+    // specific knowledge to JavaFX
+    public void initialize() {
+        this.btn2.setOnAction(e ->lbl1.setText(textInput.getText()));
+        this.btn1.setOnAction(e -> {
+            var ses = session.openSession();
+            var tx = ses.beginTransaction();
+
+            var rating = new Ratings();
+            rating.setText(textInput.getText());
+            ses.save(rating);
+
+            tx.commit();
+            ses.close();
+        });
+    }
+
+
 }
